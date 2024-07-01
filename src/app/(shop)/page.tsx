afcc55cd97a8b239bc770af60a1bffa90849eb2b
Leandro Arturi <lea.arturi@gmail.com>
2024-07-01 20:46:20 -0300

    Title Component

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,9 @@
-import { titleFont } from '@/config/fonts'
+import { Title } from '@/components'
 
 export default function HomePage() {
   return (
-    <div className=''>
-      <h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
-    </div>
+    <>
+      <Title title='Tienda' subtitle='Todos los productos' className='mb-2' />
+    </>
   )
 }
