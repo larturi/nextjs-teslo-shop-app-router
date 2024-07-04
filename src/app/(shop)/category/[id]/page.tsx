30e54eda27c5d8b0f9edc8715fe5096c2cf304d1
Leandro Arturi <lea.arturi@gmail.com>
2024-07-04 20:38:30 -0300

    Filter products by gender

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,38 @@
 import { notFound } from 'next/navigation'
 import { titleFont } from '@/config/fonts'
+import { ProductGrid, Title } from '@/components'
+import { Categories } from '@/interfaces'
+import { initialData } from '@/seed/seed'
 
 interface Props {
-  params: { id: string }
+  params: { id: Categories }
 }
+
+const seedProducts = initialData.products
 
 export default function CategoryPage({ params }: Props) {
   const { id } = params
+  const products = seedProducts.filter((product) => product.gender === id)
 
-  console.log(id)
-
-  if (id === 'kids') {
-    notFound()
+  const labels: Record<Categories, string> = {
+    men: 'Men',
+    women: 'Women',
+    kid: 'Kid',
+    unisex: 'All'
   }
-  return (
-    <div className=''>
-      <h1 className={`${titleFont.className} font-bold`}>Category Page</h1>
-    </div>
-  )
+
+  if (id === 'men' || id === 'women' || id === 'kid') {
+    return (
+      <>
+        <Title
+          title={`${labels[id]} Products`}
+          subtitle={`All products for ${labels[id]}`}
+          className='mb-2'
+        />
+        <ProductGrid products={products} />
+      </>
+    )
+  }
+
+  notFound()
 }
