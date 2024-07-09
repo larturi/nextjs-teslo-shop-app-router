bf4f943f07362e6cf77ab313fc20446d499906c0
Leandro Arturi <lea.arturi@gmail.com>
2024-07-09 12:20:25 -0300

    Seed database

--- README.md
+++ README.md
@@ -16,4 +16,5 @@
 3. Create the .env file based in .env.template
 4. Start the Postgres Container: ```docker-compose up -d```
 5. Run Prisma migrations: ```npx prisma migrate dev```
-6. Start the server: ```npm run dev```
+6. Run the seed to populate the database: ```npm run seed```
+7. Start the server: ```npm run dev```
---
+++ src/seed/seed-database.ts
@@ -0,0 +1,55 @@
+import { url } from 'inspector'
+import prisma from '../lib/prisma'
+import { initialData } from './seed'
+
+async function main() {
+  const { categories, products } = initialData
+
+  console.log('Deleting old database...')
+  await prisma.productImage.deleteMany()
+  await prisma.product.deleteMany()
+  await prisma.category.deleteMany()
+
+  console.log('Inserting Categories...')
+  const categoriesData = categories.map((category) => {
+    return { name: category }
+  })
+
+  await prisma.category.createMany({ data: categoriesData })
+
+  console.log('Inserting Products...')
+  const categoriesDb = await prisma.category.findMany()
+  const categoriesMap = categoriesDb.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id
+
+    return map
+  }, {} as Record<string, string>)
+
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type]
+      }
+    })
+
+    // Insert images
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id
+    }))
+
+    await prisma.productImage.createMany({
+      data: imagesData
+    })
+  })
+
+  console.clear()
+  console.log('Seed database executed!')
+}
+
+;(() => {
+  if (process.env.NODE_ENV === 'production') return
+  main()
+})()
